eb33b3a603e8047fe747767a6b5a9b92aefcec03
pjheeta <pjheeta.gmail.com>
2025-12-28 01:52:44 -0500

    Finished BookBot Chapter 2 : Data Analysis

---
+++ main.py
@@ -0,0 +1,19 @@
+from stats import get_num_words, countText
+
+### Function to Read Book Text from a File
+def get_book_text(bookLocation):
+    wordCount=""
+    with open(bookLocation) as f:
+        file_contents = f.read()
+    return (file_contents)
+
+##Main function
+def main():
+    bookPath = 'books/frankenstein.txt'
+    bookApplied = get_book_text(bookPath)
+    NumWords = get_num_words(bookApplied)
+    countText(bookApplied)
+    print (f"Found {NumWords} total words")
+
+
+main()
---
+++ stats.py
@@ -0,0 +1,26 @@
+
+##Function first calls lowercaseText to convert all text in the string to lowercase
+##The calls then splits the text into a list of words and counts the total number of words
+def get_num_words(splitText):
+    lowerText = lowercaseText(splitText)
+    wordCount = lowerText.split()
+    countText (wordCount)
+    print (wordCount)
+    return (len(wordCount))
+
+#Function to lowers all text in the string splitText
+def lowercaseText(splitText):
+    return splitText.lower()
+
+##go through each character in the string lowercaseText
+##passing the # of times a character appears into wordDict
+
+def countText(modText):
+    wordDict = {}
+    for lowerWord in modText:
+        for char in lowerWord:
+            wordDict[char] = wordDict.get(char, 0) + 1
+    print (wordDict)
+
+
+            
